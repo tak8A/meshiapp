7329c7eb716d7e502face68e5cbeafdc00efb0f2
天米貴裕 <amametakahiro@tenmaitakahironoMacBook-Air.local>
2021-06-18 13:38:44 +0900

    post機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  resources :posts
+  #get 'posts/index'
+  get "/" => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
