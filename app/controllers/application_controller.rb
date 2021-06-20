10c1a20d0792e6de05168954488b8e32686dd1c3
天米貴裕 <amametakahiro@tenmaitakahironoMacBook-Air.local>
2021-06-21 00:12:55 +0900

    投稿機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    add_flash_types :success, :info, :warning, :danger
+    protect_from_forgery with: :exception
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,47 @@
 class PostsController < ApplicationController
   def index
-
-    @posts = Post.all
-
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+    #Rails.logger.debug "@post : #{@post.inspect}"
+      #flash[:success] =  "投稿しました"
+      redirect_to @post, success: 'succress create task'
+
+    else
+      flash[:warning] =  "投稿に誤りがあります"
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+       flash[:notice] = "投稿を編集しました"
+       redirect_to @post
+    else  
+       render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to @post
+  end
+
 end
